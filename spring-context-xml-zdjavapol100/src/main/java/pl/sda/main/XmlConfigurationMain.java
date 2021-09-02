33570fc3d76bacf60216ac5eab6c0c00525f11d3
Michał Suwała <michal.suwala90@gmail.com>
2021-09-02 14:54:30 +0200

    added spring xml configuration example

---
+++ spring-context-xml-zdjavapol100/src/main/java/pl/sda/main/XmlConfigurationMain.java
@@ -0,0 +1,27 @@
+package pl.sda.main;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import pl.sda.model.HelloWorld;
+import pl.sda.model.Person;
+import pl.sda.service.PersonService;
+import pl.sda.service.impl.PersonServiceImpl;
+
+public class XmlConfigurationMain {
+
+    public static void main(String[] args) {
+
+        //tworzymy obiekt kontenera
+        ClassPathXmlApplicationContext ac =
+                new ClassPathXmlApplicationContext("applicationContext.xml");
+
+        HelloWorld hw = ac.getBean("helloWorld", HelloWorld.class);
+
+        hw.hello();
+
+        PersonService ps = ac.getBean("personService", PersonServiceImpl.class);
+        ps.add(new Person(1, "Jan", "Kowalski"));
+
+        System.out.println(ps.getById(1));
+    }
+
+}
---
+++ spring-context-xml-zdjavapol100/src/main/java/pl/sda/service/impl/PersonServiceImpl.java
@@ -0,0 +1,34 @@
+package pl.sda.service.impl;
+
+import pl.sda.dao.PersonDao;
+import pl.sda.model.Person;
+import pl.sda.service.PersonService;
+import pl.sda.validator.PersonValidator;
+
+public class PersonServiceImpl implements PersonService {
+
+    private final PersonDao personDao;
+
+    private PersonValidator validator;
+
+    //wstrzykiwanie przez konstruktor
+    public PersonServiceImpl(PersonDao personDao) {
+        this.personDao = personDao;
+    }
+
+    @Override
+    public void add(Person person) {
+        if (validator.isValid(person)) {
+            personDao.add(person);
+        }
+    }
+
+    @Override
+    public Person getById(Integer id) {
+        return personDao.getById(id);
+    }
+
+    public void setValidator(PersonValidator validator) {
+        this.validator = validator;
+    }
+}
